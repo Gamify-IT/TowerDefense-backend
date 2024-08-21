64ec450ed0a28a8c121421a29a0eb0f91aeac88e
Leonard <weasleywalker@gmx.de>
2024-08-21 15:58:15 +0200

    added mvnm files and main classes

---
+++ src/main/java/de/unistuttgart/towerdefensebackend/Constants.java
@@ -0,0 +1,28 @@
+package de.unistuttgart.towerdefensebackend;
+
+/**
+ * This class contains constants for defensive field checks
+ * e.g. the game time cannot be smaller than 0 seconds
+ */
+public class Constants {
+
+    //----Game configuration attributes----
+
+    //game time in seconds
+    public static final int MIN_TIME = 0;
+
+    //----Game result attributes----
+    public static final int MIN_POINTS = -600;
+    public static final int MAX_POINTS = 600;
+    public static final int MIN_QUESTION_COUNT = 0;
+    public static final int MAX_QUESTION_COUNT = 600;
+
+    //----Overworld result attributes----
+
+    //score saved in %
+    public static final long MIN_SCORE = 0;
+    public static final long MAX_SCORE = 100;
+
+    private Constants() {}
+
+}
