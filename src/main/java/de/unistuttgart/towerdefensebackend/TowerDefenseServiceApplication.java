768292824d2cc1cce00a245d03524009c0b3f679
Leonard <weasleywalker@gmx.de>
2024-08-21 15:29:16 +0200

    set up pom.xml

--- src/main/java/de/unistuttgart/towerdefensebackend/TowerDefenseServiceApplication.java
+++ src/main/java/de/unistuttgart/towerdefensebackend/TowerDefenseServiceApplication.java
@@ -2,7 +2,8 @@ package de.unistuttgart.towerdefensebackend;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class TowerDefenseServiceApplication {
+
     public static void main(String[] args) {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
@@ -10,7 +11,6 @@ public class Main {
 
         // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
             // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Strg+F8.
             System.out.println("i = " + i);
